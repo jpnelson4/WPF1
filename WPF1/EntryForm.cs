c9b7f1622babe5563cbe4e92b04fc4973ce15a04
James Nelson <jpnelson@ou.edu>
2019-02-07 17:36:50 -0600

    Finished Participation 1

---
+++ WPF1/EntryForm.cs
@@ -0,0 +1,36 @@
+﻿//James Nelson
+//Connor
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+            this.Name=name;
+            this.Address=address;
+            this.ZipCode=zipCode;
+        }
+        public override string ToString() 
+        {
+            return $"Name: {Name}, Address: {Address}, ZipCode: {ZipCode}";
+        }
+
+    }
+}
--- WPF1/MainWindow.xaml.cs
+++ WPF1/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//James Nelson
+//Connor 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +24,25 @@ namespace WPF1
     {
         public MainWindow()
         {
+
             InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm instance = new EntryForm(txtName.Text, txtAddress.Text, Convert.ToInt32(txtZip.Text));
+            lst.Items.Add(instance);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
+
